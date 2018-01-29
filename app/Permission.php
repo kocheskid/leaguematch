e7832872a6f7e310c673f202f496d027fbf7959e
Daniel Kocheski <daniel.kocheski@neocom.com.mk>
2018-01-29 23:20:08 +0100

    Permission fix

--- app/Permission.php
+++ app/Permission.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
-//    public static function defaultPermissions()
-//    {
-//        return [
-//            'viewPost',
-//            'addPost',
-//            'editPost',
-//            'deletePost',
-//        ];
-//    }
+    public static function defaultPermissions()
+    {
+        return [
+            'viewPost',
+            'addPost',
+            'editPost',
+            'deletePost',
+        ];
+    }
 }
